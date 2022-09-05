3bbbeed329bc6414f2dd2b75b9e8e381a86ba982
aladnansami <aladnansami21@gmail.com>
2022-09-05 14:32:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>This is mine</h1>
+         
         </p>
         <a
           className="App-link"
